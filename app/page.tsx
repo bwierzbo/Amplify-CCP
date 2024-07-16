d3eced15f4753a9d2fd019a4081717147d85936a
Benjamin Wierzbanowski <39303255+bwierzbo@users.noreply.github.com>
2024-07-16 15:44:56 -0400

    not sure

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import outputs from "@/amplify_outputs.json";
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
