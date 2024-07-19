6083840ee4b87f111e7e055ea5227b838d38d0b9
Benjamin Wierzbanowski <39303255+bwierzbo@users.noreply.github.com>
2024-07-19 17:24:03 -0400

    removed unused file

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,11 +1,54 @@
+"use client";
+
 import { fetchRevenue } from '@/app/lib/data';
+import outputs from "@/amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import { useState, useEffect } from "react";
 
+Amplify.configure(outputs);
 
-export default async function Page() {
-  const revenue = await fetchRevenue(); // Fetch data inside the component
+const client = generateClient<Schema>();
+
+export default function Page() {
+  const [revenue, setRevenue] = useState<Array<Schema["Revenue"]["type"]>>([]);
+
+  function listTodos() {
+    client.models.Revenue.observeQuery().subscribe({
+      next: (data) => setRevenue([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createRevenue() {
+
+    client.models.Revenue.create({
+      month: window.prompt("month content"),
+      revenue: 500,
+    });
+  }
 
     return (
     
-    <p>Dashboard Page</p>
+      <main>
+      <h1>My todos</h1>
+      <button onClick={createRevenue}>+ new</button>
+      <ul>
+        {revenue.map((revenue) => (
+          <li key={revenue.id}>{revenue.month} {revenue.revenue}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+          Review next steps of this tutorial.
+        </a>
+      </div>
+    </main>
     );
   }
