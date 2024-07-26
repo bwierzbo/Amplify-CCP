419f4f6917fa9fb69416e3df4a9cb6b9cb3c40c7
Benjamin Wierzbanowski <39303255+bwierzbo@users.noreply.github.com>
2024-07-26 18:40:04 -0400

    side nav management

--- app/dashboard/management/page.tsx
+++ app/dashboard/management/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <p>Cider Page</p>;
+    return <p>Management Dashboard Page</p>;
   }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,7 +11,9 @@ import {
   CurrencyDollarIcon,
   CogIcon,
   ChevronDownIcon,
-  ChevronUpIcon
+  ChevronUpIcon,
+  ClipboardDocumentIcon,
+  DocumentTextIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -38,67 +40,94 @@ const links: NavLinkType[] = [
   {
     name: 'Management',
     href: '/dashboard/management',
-    icon: BeakerIcon,
+    icon: ClipboardDocumentIcon,
     children: [
-      { name: 'Suppliers', href: '/dashboard/management/suppliers' },
-      { name: 'Inventory', href: '/dashboard/management/inventory' },
+      { name: 'Suppliers', href: '/dashboard/management/suppliers', icon: TruckIcon },
+      { name: 'Inventory', href: '/dashboard/management/inventory', icon: DocumentDuplicateIcon },
     ],
   },
   {
     name: 'Sales',
-    href: '/dashboard/sales',
+    href: '/dashboard/finances',
     icon: CurrencyDollarIcon,
     children: [
-      { name: 'Orders', href: '/dashboard/sales/orders' },
-      { name: 'Invoices', href: '/dashboard/sales/invoices' },
+      { name: 'Customers', href: '/dashboard/finances/customers', icon: UserGroupIcon },
+      { name: 'Invoices', href: '/dashboard/finances/invoices', icon: DocumentTextIcon },
     ],
   },
 ];
 
-function NavLink({ link }: { link: NavLinkType }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname();
+function NavLink({
+  link,
+  isOpen,
+  onClick,
+  activePath,
+}: {
+  link: NavLinkType;
+  isOpen: boolean;
+  onClick: () => void;
+  activePath: string;
+}) {
   const LinkIcon = link.icon;
-  const isActive = pathname === link.href;
-
-  const toggleSubmenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const isActive = activePath === link.href || (link.name !== 'Home' && activePath.startsWith(link.href));
 
   return (
-    <>
-      <div className="flex items-center justify-between">
+    <div className="w-full">
+      <div
+        className="flex items-center justify-between w-full"
+        onClick={link.children ? onClick : undefined}
+      >
         <Link
           href={link.href}
           className={clsx(
-            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            'flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
             {
               'bg-sky-100 text-blue-600': isActive,
-            },
+            }
           )}
-          onClick={link.children ? toggleSubmenu : undefined}
+          onClick={() => !link.children && onClick()}
         >
           {LinkIcon && <LinkIcon className="w-6" />}
           <p className="hidden md:block">{link.name}</p>
-          {link.children && (isOpen ? <ChevronUpIcon className="w-5" /> : <ChevronDownIcon className="w-5" />)}
+          {link.children &&
+            (isOpen ? <ChevronUpIcon className="w-5" /> : <ChevronDownIcon className="w-5" />)}
         </Link>
       </div>
       {isOpen && link.children && (
         <div className="ml-4">
           {link.children.map((child) => (
-            <NavLink key={child.name} link={child} />
+            <NavLink
+              key={child.name}
+              link={child}
+              isOpen={activePath.startsWith(child.href)}
+              onClick={onClick}
+              activePath={activePath}
+            />
           ))}
         </div>
       )}
-    </>
+    </div>
   );
 }
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  const [openTab, setOpenTab] = useState<string | null>(null);
+
+  const handleToggle = (tabName: string) => {
+    setOpenTab((prevOpenTab) => (prevOpenTab === tabName ? null : tabName));
+  };
+
   return (
     <>
       {links.map((link) => (
-        <NavLink key={link.name} link={link} />
+        <NavLink
+          key={link.name}
+          link={link}
+          isOpen={openTab === link.name || pathname.startsWith(link.href)}
+          onClick={() => handleToggle(link.name)}
+          activePath={pathname}
+        />
       ))}
     </>
   );
